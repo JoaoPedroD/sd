6dec72004b754106cfa3caae11f524b1394ace7a
Joao Pedro <jp.gandarela@hotmail.com>
2020-09-09 11:53:51 -0300

    melhoras

--- Semana_2/ativo.py
+++ Semana_2/ativo.py
@@ -4,11 +4,16 @@ PORT = 8000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 s.connect(dest)
-# while True:
-data = input()
-res = bytes(data, 'utf-8')
-s.send(res)
-echo = s.recv(4096)
-if echo:
-    print(echo.decode("utf-8"))
-    s.close()
+while True:
+    data = input("Entre um arquivo ou uma lista de arquivos separado por virgula\nPara sair da aplicação aperte enter\n")
+    if data:
+        # enviando a mensage com os arquivos para o servidor
+        res = bytes(data, 'utf-8')
+        s.send(res)
+        # # recebendo a mensage com os arquivos para o servidor
+        echo = s.recv(4096)
+        if echo:
+            print(echo.decode("utf-8"))
+    else:
+        s.close()
+        break
--- Semana_2/processamento/server.py
+++ Semana_2/processamento/server.py
@@ -12,40 +12,50 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(1)
 while True:
-        new_connection, add = s.accept()
+    new_connection, add = s.accept()
+    while True:
         try:
             data = new_connection.recv(4096)
-            data = data.decode("utf-8")
-            data = data.split(",")
-            r=""
-            for d in data:
-                dicio = {}
-                d = d.strip(" ")
-                if d:
-                    r+=d+":\n"
-                    try:
-                        arq = openFile(os.getcwd()+"/"+d)
-                    except Exception:
-                        r+=f"\tO arquivo {d} não foi encontrado\n"
-                        continue
-                    arq = arq.split("\n")
-                    for linha in arq:
-                        if linha:
-                            linha = linha.lower()
-                            linha = linha.strip(" ")
-                            linha = linha.strip("\n")
-                            linha = re.compile('[%s]' % re.escape(string.punctuation)).sub("",linha)
-                            palavras = linha.split(" ")
-                            for p in palavras:
-                                if p:
-                                    if p in list(dicio):
-                                        dicio[p] = dicio[p] + 1
-                                    else:
-                                        dicio[p] = 1
-                    dicio=sorted(dicio.items(),  key=lambda x: x[1], reverse=True)
-                    for i, k in zip(range(0,10),dicio):
-                        r += "\t"+k[0]+": "+str(k[1])+"\n"
-                    r+="\n"
-            new_connection.send(bytes(r, 'utf-8'))
+            # testando se o cliente fechou a conexao
+            if not data:
+                new_connection.close()
+                break
+            else:
+                data = data.decode("utf-8")
+                data = data.split(",")
+                r=""
+                # percorrendo a possivel lista de arquivos
+                for d in data:
+                    dicio = {}
+                    d = d.strip(" ")
+                    if d:
+                        r+=d+":\n"
+                        try:
+                            # solicitando da camada de dados o conteudo o arquivo
+                            arq = openFile(os.getcwd()+"/"+d)
+                        except Exception:
+                            r+=f"\tO arquivo {d} não foi encontrado\n"
+                            continue
+                        arq = arq.split("\n")
+                        for linha in arq:
+                            if linha:
+                                linha = linha.lower()
+                                linha = linha.strip(" ")
+                                linha = linha.strip("\n")
+                                linha = re.compile('[%s]' % re.escape(string.punctuation)).sub("",linha)
+                                palavras = linha.split(" ")
+                                for p in palavras:
+                                    if p:
+                                        if p in list(dicio):
+                                            dicio[p] = dicio[p] + 1
+                                        else:
+                                            dicio[p] = 1
+                        dicio=sorted(dicio.items(),  key=lambda x: x[1], reverse=True)
+                        # montando a resposta para o cliente
+                        for i, k in zip(range(0,10),dicio):
+                            r += "\t"+k[0]+": "+str(k[1])+"\n"
+                        r+="\n"
+                new_connection.send(bytes(r, 'utf-8'))
         except:
+            # caso aconteça algum erro, o servidor comunica o problema pede novamente o arquivo
             new_connection.send(bytes("Tivemos algum problema, tente novamente", 'utf-8'))
